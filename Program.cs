f86c3ce1ee20942476402addc38e81321c5a6cd6
Dima Maltsev <dima_maltsev@hotmail.com>
2026-04-11 17:49:10 -0500

    Added SubstringVsSlice Benchmark Tests

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using BenchmarkDotNet.Running;
+
+Console.WriteLine("Start...");
+
+// Run the Substring vs Slice Benchmark Test
+_ = BenchmarkRunner.Run<Companova.BenchmarkTests.SubstringVsSlice>();
+
+Console.Read();
